f4af6cbfc2c92b0562bc73e3c6cbb17add003630
Ruan Oliveira <148791318+ruanoliveira93@users.noreply.github.com>
2025-04-04 19:43:26 -0300

    Frontend e Conexão Database

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,36 +1,46 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Inicio from '../../Pages/Inicio/Inicio'
-import LivrosDoados from '../../Pages/LivrosDoados/LivrosDoados'
-import QueroDoar from '../../Pages/QueroDoar/QueroDoar'
-import logoLivro from '../../assets/Logo Livro.png'
-import s from './header.module.scss'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Inicio from "../../Pages/Inicio/Inicio";
+import LivrosDoados from "../../Pages/LivrosDoados/LivrosDoados";
+import QueroDoar from "../../Pages/QueroDoar/QueroDoar";
+import logoLivro from "../../assets/Logo Livro.png";
+import s from "./header.module.scss";
+import SearchForm from "../SearchForm/SearchForm";
+import Pesquisa from "../../Pages/Pesquisa/Pesquisa";
 
 export default function Header() {
+  return (
+    <BrowserRouter>
+      <header>
+        <section className={s.logoHeader}>
+          <Link to="/">
+            <img src={logoLivro} alt="Imagem ilustrativa de um livro aberto" />
+            <h1>Livros Vai na Web</h1>
+          </Link>
+        </section>
 
-    return (
-        <BrowserRouter>
-            <header>
-                <section className={s.logoHeader}>
-                    <Link to='/'>
-                        <img src={logoLivro} alt="Imagem ilustrativa de um livro aberto" />
-                        <h1>Livros Vai na Web</h1>
-                    </Link>
-                </section>
+        <nav className={s.navbar}>
+          <ul>
+            <li>
+              <Link to="/">Início</Link>
+            </li>
+            <li>
+              <Link to="/livrosdoados">Livros Doados</Link>
+            </li>
+            <li>
+              <Link to="/querodoar">Quero Doar</Link>
+            </li>
+          </ul>
+        </nav>
 
-                <nav className={s.navbar}>
-                    <ul>
-                        <li><Link to='/'>Início</Link></li>
-                        <li><Link to='/livrosdoados'>Livros Doados</Link></li>
-                        <li><Link to='/querodoar'>Quero Doar</Link></li>
-                    </ul>
-                </nav>
-            </header>
+        <SearchForm />
+      </header>
 
-            <Routes>
-                <Route index element={<Inicio />} />
-                <Route path='/livrosdoados' element={<LivrosDoados />} />
-                <Route path='/querodoar' element={<QueroDoar />} />
-            </Routes>
-        </BrowserRouter>
-    )
+      <Routes>
+        <Route index element={<Inicio />} />
+        <Route path="/livrosdoados" element={<LivrosDoados />} />
+        <Route path="/querodoar" element={<QueroDoar />} />
+        <Route path="/pesquisa" element={<Pesquisa />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
