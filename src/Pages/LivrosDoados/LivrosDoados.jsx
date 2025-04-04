b36559fecc14a5ab8f2b2bb2eab47eb73ce8680a
Ruan Oliveira <148791318+ruanoliveira93@users.noreply.github.com>
2025-04-04 19:41:58 -0300

    Frontend e Conexão Database

--- src/Pages/LivrosDoados/LivrosDoados.jsx
+++ src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import SearchForm from "../../Components/SearchForm/SearchForm";
+import SearchLivros from "../../Components/SearchLivros/SearchLivros";
 import s from "./livrosdoados.module.scss";
 
 export default function LivrosDoados() {
@@ -11,8 +11,10 @@ export default function LivrosDoados() {
   // Função para buscar livros da API
   const buscarLivros = async () => {
     try {
-      const response = await axios.get("https://api-livros-vainaweb-vp00.onrender.com/livros");
-      console.log("Livros carregados com sucesso!", response.data);
+      const response = await axios.get("http://127.0.0.1:5000/livros");
+      console.log(
+        `Livros carregados com sucesso! (Status: ${response.status})`
+      );
 
       let itens = response.data;
       itens.sort((a, b) => a.titulo.localeCompare(b.titulo));
@@ -26,32 +28,45 @@ export default function LivrosDoados() {
     }
   };
 
-  // Buscar livros ao carregar o componente
+  // Buscar livros ao carregar a função
   useEffect(() => {
     buscarLivros();
   }, []);
 
-  // Função para filtrar livros
+  // Filtro de pesquisa
   const handleSearch = (query) => {
     if (!query.trim()) {
-      setLivrosFiltrados(livros); // Se o campo estiver vazio, exibe todos os livros
+      setLivrosFiltrados(livros);
       return;
     }
 
     const resultado = livros.filter(
       (livro) =>
         livro.titulo.toLowerCase().includes(query.toLowerCase()) ||
-        livro.autor.toLowerCase().includes(query.toLowerCase())
+        livro.autor.toLowerCase().includes(query.toLowerCase()) ||
+        livro.categoria.toLowerCase().includes(query.toLowerCase())
     );
 
     setLivrosFiltrados(resultado);
   };
 
+  // Função para deletar livro por query params
+  const deleteLivros = async (id) => {
+    try {
+        await axios.delete(`http://127.0.0.1:5000/livros?id=${id}`)
+        alert("Livro deletado com sucesso!")
+        buscarLivros()
+    } catch (err) {
+        console.error("Livro não encontrado.", err)
+        alert("Erro ao deletar o livro. Veja o console para mais detalhes.");
+    }
+  }
+
   return (
     <section className={s.containerLivros}>
       <h2>Livros Doados</h2>
       {erro && <p style={{ color: "red" }}>{erro}</p>}
-      <SearchForm onSearch={handleSearch} />
+      <SearchLivros onSearch={handleSearch} />
 
       <button
         onClick={buscarLivros}
@@ -64,31 +79,34 @@ export default function LivrosDoados() {
 
       <p>Total de livros: ({livrosFiltrados.length})</p>
 
-
       <section className={s.livros}>
         {livrosFiltrados.length > 0 ? (
-          livrosFiltrados.map((livro) => (
-            <div className={s.livro} key={livro.id}>
+          livrosFiltrados.map((item) => (
+            <section key={item.id} className={s.livro}>
               <img
-                src={livro.imagem_url || "https://via.placeholder.com/150"}
-                alt={`Capa do livro ${livro.titulo}`}
-                onError={(e) =>
-                  (e.target.src = "https://via.placeholder.com/150")
+                src={
+                  item.imagem_url ||
+                  "https://placehold.jp/3d4070/ffffff/245x340.png"
                 }
+                onError={(e) =>
+                  (e.target.src =
+                    "https://placehold.jp/3d4070/ffffff/245x340.png")
+                }
+                alt={`Capa do livro ${item.titulo}`}
               />
-              <div>
-                <h3
-                  style={{ fontWeight: 600 }}
-                  title={`Título: ${livro.titulo}`}
-                >
-                  {livro.titulo}
-                </h3>
-                <p title={`Autor: ${livro.autor}`}>Autor: {livro.autor}</p>
-                <p title={`Categoria: ${livro.categoria}`}>
-                  Categoria: {livro.categoria}
-                </p>
-              </div>
-            </div>
+              <section className={s.texto}>
+                <h3 title={`Título: ${item.titulo}`}>{item.titulo}</h3>
+                <p title={`Autor: ${item.autor}`}>{`Autor: ${item.autor}`}</p>
+                <p
+                  title={`Categoria: ${item.categoria}`}
+                >{`Categoria: ${item.categoria}`}</p>
+              </section>
+              <section className={s.modalDelete}>
+                <button onClick={() => deleteLivros(item.id)} type="button">
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              </section>
+            </section>
           ))
         ) : (
           <p>Nenhum livro encontrado.</p>
@@ -97,4 +115,3 @@ export default function LivrosDoados() {
     </section>
   );
 }
-
