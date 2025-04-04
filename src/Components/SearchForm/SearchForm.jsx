2e4f2ebfb6f11a1d55c158de762c00cb86bda7a2
Ruan Oliveira <148791318+ruanoliveira93@users.noreply.github.com>
2025-04-04 19:44:05 -0300

    Frontend e Conexão Database

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,29 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import LupaPesquisa from "../../assets/search.png";
+import { useNavigate } from "react-router";
 import s from "./searchForm.module.scss";
 
-export default function SearchForm({ onSearch }) {
+export default function SearchForm() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const formRef = useRef(null);
 
-  const handleSearch = () => {
-    if (!query.trim()) return; // Evita pesquisa vazia
-    onSearch(query); // Envia a pesquisa para o componente pai
-    setQuery(""); // Limpa o campo após a busca
+  const handleFocus = () => {
+    if (formRef.current) {
+      formRef.current.style.border = "1px solid white"
+    }
+  };
+
+  const handleBlur = () => {
+    if (formRef.current) {
+      formRef.current.style.border = "1px solid #005695"
+    }
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!query.trim()) return;
+    setQuery("");
+    navigate("/pesquisa", { state: { texto: query } });
   };
 
   return (
-    <section className={s.formSearch}>
+    <form ref={formRef} className={s.formSearch} onSubmit={handleSearch}>
       <input
         type="search"
         placeholder="O que você procura?"
         value={query}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={(e) => e.key === "Enter" && handleSearch()} // Pesquisa ao pressionar "Enter"
       />
 
-      <button onClick={handleSearch} type="button">
+      <button onClick={handleSearch} type="submit">
         <img src={LupaPesquisa} alt="Imagem Lupa" />
       </button>
-    </section>
+    </form>
   );
 }
